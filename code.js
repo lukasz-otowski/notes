e50d9c4575da45c5eaa44ab6edc084ea271a7976
Łukasz Otowski <lukaszotowski@interia.pl>
2018-08-23 20:33:54 +0200

    init

---
+++ code.js
@@ -0,0 +1,18 @@
+//BASIC
+//DOCUMENT METHODS
+var name = 'hey';//string
+var name = 4;//intiger
+var name = [1,2,3];//array
+let name = 'value';//create local variable
+const name = 'value';//create not changeable value
+//DOCUMENT METHODS
+document.getElementById('id-name');
+document.getElementsByClassName('classes names');
+document.getElementsByTagName('tag-name');
+document.querySelector('selected-element');
+document.querySelectorAll('selected-element');
+document.head;//method what contains methods for head, or get value about head tag
+document.body;//method what contains methods for body, or get value about body tag
+document.URL;
+document.doctype;
+document.characterSet;//get value of charset type
