29390e4e72dd16b679d91d188b3a59983b6812a2
Łukasz Otowski <lukaszotowski@interia.pl>
2018-08-24 19:35:55 +0200

    init

---
+++ code.php
@@ -0,0 +1,31 @@
+<?php //php open tag
+$name = value;
+$this-> value;
+var_dump();
+public
+protected
+namespace
+new
+use
+require_once
+explode('sign', value);
+__DIR__ . 
+map('method', '/', 'path', 'value');
+//DONE CODE LINES
+$this->match = $router->match();
+require_once __DIR__ . '/../app/routing/routes.php';
+if(!isset($_SESSION)) session_start();//start session if not already started
+new \App\RouteDispatcher($router);
+var_dump($user->toArray());
+define('BASE_PATH', realpath(__DIR__.'/../../'));
+explode('@', $this->match['target']);
+$router->map('GET', '/', 'App\Controllers\IndexController@show', 'home');
+//TOOLS
+//composer
+//brew
+//LIBRARIES
+//composer require vlucas/phpdotenv
+//composer require altorouter/altorouter
+//composer require philo/laravel-blade
+//composer require phpmailer/phpmailer
+//composer require illuminate/database
