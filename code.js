1c4ccc237aeaa1f519a794fd60d15bc5dd8d8e33
Łukasz Otowski <lukaszotowski@interia.pl>
2018-08-30 08:45:11 +0200

    added toPrecision and toFixed

--- code.js
+++ code.js
@@ -13,6 +13,8 @@ document.querySelector('selected-element');
 document.querySelectorAll('selected-element');
 document.head;//method what contains methods for head, or get value about head tag
 document.body;//method what contains methods for body, or get value about body tag
-document.URL;
-document.doctype;
+document.URL;//actual URL address
+document.doctype;//declaration type
 document.characterSet;//get value of charset type
+4.499.toPrecision(2);//"4.5"
+4.499.toFixed(2);//"4.50"
