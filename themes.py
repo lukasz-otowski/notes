5c35e8922867b3348fb4c151137f9ed70c4cbd01
Łukasz Otowski <lukaszotowski@interia.pl>
2018-10-25 23:03:38 +0200

    add themes.py

---
+++ themes.py
@@ -0,0 +1,247 @@
+cognitive_science
+	philosophy
+	psychology
+	artificial_intelligence
+	neurobiology
+		brain
+			cerebrum
+				cerebral_cortex
+					frontal_lobe
+						brocas_area
+							spech
+					parietal_lobe
+					temporal_lobe
+						superior_temporal_lobe
+							wernickes_area
+								spech
+					occipital_lobe
+			neurons
+				dendrit
+				nucleus
+				soma
+				axon
+				myelin
+					myelin_sheath
+				schwann
+				ranvier_node
+				axon_terminal
+			glial_cells
+	antropology
+	linguistics
+		chinesse
+			mandarin
+				numbers
+					'one 一'
+					'two 二'
+					'three 三'
+					'four 四'
+					'five 五'
+					'six 六'
+					'seven 七'
+					'eight 八'
+					'nine 九'
+					'ten 十'
+genetics
+webdev
+	html
+	css
+		property_flexbox
+			"display:flex;"
+				"display:-webkit-box;"
+				"display:-moz-box;"
+				"display:-ms-flexbox;"
+				"display:-webkit-flex;"
+			"display:flex/inline-flex;"#display as flex container
+			"flex-direction:row/column;"#set all horizontal/vertical
+			"order:number;"#change order in queue
+			"flex-wrap:wrap/nowrap/wrapreverse;"#wrap lines if over of container
+			"justify-content: flex-start/flex-end/center/space-between/space-around/space-evenly;"#auto creating margins
+			"align-self:flex-start/flex-end/center;"#vertical self alignment of specific item
+			"align-items:flex-start/flex-end/center/stretch;"#vertical alignment of content
+			"align-content:flex-start/flex-end/center;"
+			"flex-grow:number;"
+			"flex-shrink:number;"
+			"flex-basis:length;"#width property for flexbox
+			"flex:number/numbers;"#define grow or shrink of one or more elements in flex container
+		property_grid
+	javascript
+		data_types
+			var name = 'hey';//string
+			var name = 4;//intiger
+			var name = [1,2,3];//array
+			let name = 'value';//create local variable
+			const name = 'value';//create not changeable value
+		operators
+			12%5;#remainder operator return an result of rest from two divided number
+		methods
+			document
+				document.getElementById('id-name');
+				document.getElementsByClassName('classes names');
+				document.getElementsByTagName('tag-name');
+				document.querySelector('selected-element');
+				document.querySelectorAll('selected-element');
+				document.head;#ethod what contains methods for head, or get value about head tag
+				document.body;#method what contains methods for body, or get value about body tag
+				document.URL;#actual URL address
+				document.doctype;#declaration type
+				document.characterSet;#get value of charset type
+			number
+				4.499.toPrecision(2);#4.5
+				4.499.toFixed(2);#4.50
+			array
+				array.length;#number of array items
+				array.push("item", "item2");#add new items into array
+				array.indexOf(value);#index of item from array
+				array.map(method);#use a method on all items of array
+				array.forEach(function(element){});#function execution on all element for chosen array
+		keyboard
+			let x = event.keyCode;
+			keycode
+				backspace 	8
+				tab 	9
+				enter 	13
+				shift 	16
+				ctrl 	17
+				alt 	18
+				pause/break 	19
+				caps lock 	20
+				escape 	27
+				page up 	33
+				page down 	34
+				end 	35
+				home 	36
+				left arrow 	37
+				up arrow 	38
+				right arrow 	39
+				down arrow 	40
+				insert 	45
+				delete 	46
+				0 	48
+				1 	49
+				2 	50
+				3 	51
+				4 	52
+				5 	53
+				6 	54
+				7 	55
+				8 	56
+				9 	57
+				a 	65
+				b 	66
+				c 	67
+				d 	68
+				e 	69
+				f 	70
+				g 	71
+				h 	72
+				i 	73
+				j 	74
+				k 	75
+				l 	76
+				m 	77
+				n 	78
+				o 	79
+				p 	80
+				q 	81
+				r 	82
+				s 	83
+				t 	84
+				u 	85
+				v 	86
+				w 	87
+				x 	88
+				y 	89
+				z 	90
+				left window key 	91
+				right window key 	92
+				select key 	93
+				numpad 0 	96
+				numpad 1 	97
+				numpad 2 	98
+				numpad 3 	99
+				numpad 4 	100
+				numpad 5 	101
+				numpad 6 	102
+				numpad 7 	103
+				numpad 8 	104
+				numpad 9 	105
+				multiply 	106
+				add 	107
+				subtract 	109
+				decimal point 	110
+				divide 	111
+				f1 	112
+				f2 	113
+				f3 	114
+				f4 	115
+				f5 	116
+				f6 	117
+				f7 	118
+				f8 	119
+				f9 	120
+				f10 	121
+				f11 	122
+				f12 	123
+				num lock 	144
+				scroll lock 	145
+				semi-colon 	186
+				equal sign 	187
+				comma 	188
+				dash 	189
+				period 	190
+				forward slash 	191
+				grave accent 	192
+				open bracket 	219
+				back slash 	220
+				close braket 	221
+				single quote 	222 
+	vue
+	firebase
+	php
+python
+	count = len(open('themes.py', 'r').readlines())#get line count of file 'themes.py'
+	import 
+		linecache
+			theline = linecache.getline('themes.py', 7)#read line number 7 from file called 'themes.py'
+	django
+git
+	git add the_left_out_file
+	git commit --amend --no-edit #add new added files to last commit
+graphic
+	white
+	black
+	blue
+	red
+	green
+music
+	sounds
+data_science
+	algorithm
+		insertion_sort
+		merge_sort
+		bubble_sort
+		binary_tree
+			depth_first_traversal
+				pre_order_traversal#visit the parent first and then left and right children;
+				in_order_traversal#visit the left child, then the parent and the right child;
+				post_order_traversal#visit left child, then the right child and then the parent;
+			breadth_first_traversal
+				level_order_traversal
+	machine_learning
+		deep_learning
+			neural_network
+	statistics
+		linear_regression
+computer_hardware
+	device
+		motherboard_device
+			chipset 
+				northbridge
+				southbridge
+			data_storage
+				central_processing_unit
+					 microprocessor
+				hard_disk_drive
+				solid_state_drive
+				random_access_memory
+		peripheral_device
