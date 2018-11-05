0fe1b557af91741c336651ee078fe2ad89fd5c8f
Łukasz Otowski <lukaszotowski@interia.pl>
2018-11-05 11:33:41 +0100

    update html for head and svg

--- themes.py
+++ themes.py
@@ -300,6 +300,20 @@ programming
         abstraction#abstraction is a way to minimize complexity and allow optimal design and implementation software. It hide technical raw things in easier APIs.
 webdev
 	html
+		head
+			'<head>'
+				'<meta charset="UTF-8">'
+				'<meta name="viewport" content="width=device-width, initial-scale=1.0">'
+				'<meta http-equiv="X-UA-Compatible" content="ie=edge">'
+			'</head>'
+		svg
+			'<svg width="600" height="600">'
+				'<rect x="300" y="100" fill="blue" width="100" height="200"></rect>'
+				'<circle cx="200" cy="200" r="50" fill="pink" stroke="red" stroke-width="2" ></circle>'
+				'<line x1="100" y1="100" x2="120" y2="300" stroke="grey" stroke-width="2"></line>'
+				'<path d="M 150 50 L 75 200 L 225 200 Z" fill="orange"></path>'
+				'<path d="M 150 50 L 75 200 L 225 200 C 225 200 150 150 150 50" fill="green"></path>'
+			'</svg>'
 	css
 		property_flexbox
 			"display:flex;"
